a685de3f5a62d61d80e6ebc7cfcae25cfcf58481
jean-knapp <email+github@jeankn.app>
2022-09-06 18:42:56 -0300

    Updated docs.

--- Probability/normcdf.m
+++ Probability/normcdf.m
@@ -1,26 +1,33 @@
-% normcdf
-
+%% NORMCDF
 % Returns the cdf of the normal distribution with mean mu and standard
 % deviation sigma, evaluated at the values in x.
+%
+% Author: *Jean Knapp* 
 
-% Inputs
+%% Inputs
+% 
+% * x       Values at which to evaluate the pdf, specified as a scalar 
+%           value or an array of scalar values.
+% * mu      Mean of the normal distribution, specified as a scalar value or 
+%           an array of scalar values.
+% * sigma   Standard deviation of the normal distribution, specified as a 
+%           positive scalar value or an array of positive scalar values.
+% 
 
-    % x     Values at which to evaluate the pdf, specified as a scalar
-    %       value or an array of scalar values.
+%% Outputs
+%
+% * y       cdf values, evaluated at the values in x, returned as a scalar 
+%           value or an array of scalar values. p is the same size as x, 
+%           mu, and sigma after any necessary scalar expansion. Each 
+%           element in p is the cdf value of the distribution specified by 
+%           the corresponding elements in mu and sigma, evaluated at the 
+%           corresponding element in x.
+%
 
-    % mi    Mean of the normal distribution, specified as a scalar value 
-    %       or an array of scalar values.
+%% Implementation
 
-    % sigma Standard deviation of the normal distribution, specified as a 
-    %       positive scalar value or an array of positive scalar values.
+function y = normcdf(x, mu, sigma)
 
-% Outputs
-
-    % y     pdf values, evaluated at the values in x, returned as a scalar 
-    %       value or an array of scalar values.
-
-function y = normcdf(x, mi, sigma)
-
-    y = 1/2 - erf(sqrt(2) * (mi - x) / (2 * sqrt(sigma^2)))/2;
+    y = 1/2 - erf(sqrt(2) * (mu - x) / (2 * sqrt(sigma^2)))/2;
 
 end
--- Probability/normpdf.m
+++ Probability/normpdf.m
@@ -1,26 +1,29 @@
-% normpdf
-
-% Returns the pdf of the normal distribution with mean mu and standard 
+%% NORMCDF
+% Returns the pdf of the normal distribution with mean mu and standard
 % deviation sigma, evaluated at the values in x.
+%
+% Author: *Jean Knapp* 
 
-% Inputs
+%% Inputs
+% 
+% * x       Values at which to evaluate the pdf, specified as a scalar 
+%           value or an array of scalar values.
+% * mu      Mean of the normal distribution, specified as a scalar value or
+%           an array of scalar values.
+% * sigma   Standard deviation of the normal distribution, specified as a 
+%           positive scalar value or an array of positive scalar values.
+% 
 
-    % x     Values at which to evaluate the pdf, specified as a scalar 
-    %       value or an array of scalar values.
+%% Outputs
+%
+% * y       pdf values, evaluated at the values in x, returned as a scalar 
+%           value or an array of scalar values.
+%
 
-    % mi    Mean of the normal distribution, specified as a scalar value
-    %       or an array of scalar values.
+%% Implementation
 
-    % sigma Standard deviation of the normal distribution, specified as a
-    %       positive scalar value or an array of positive scalar values.
+function y = normpdf(x, mu, sigma)
 
-% Outputs
-
-    % y     pdf values, evaluated at the values in x, returned as a scalar 
-    %       value or an array of scalar values.
-
-function y = normpdf(x, mi, sigma)
-
-    y = 1 / (sigma * sqrt(2*pi)) * exp(-(x-mi)^2 / (2 * sigma^2));
+    y = 1 / (sigma * sqrt(2*pi)) * exp(-(x-mu)^2 / (2 * sigma^2));
 
 end
